85f509736367fb91b5b6e3d9c7ef143a393045a2
pl23 <61396518+pl23@users.noreply.github.com>
2023-06-01 23:38:34 +0000

    a

--- 0.00.01/new.py
+++ 0.00.01/new.py
@@ -1,4 +1,11 @@
-def new_player ():
+def new_player():
     def info_get ():
-        input1 = input ('')
+        input1 = input ('a :')
+        math1 = input1.isdigit()   
+        if math1 == False:
+            print('error not a digit')
+            
+        else
+
     info_get()
+new_player()
