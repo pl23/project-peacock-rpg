4652f98a4d00c8f2621f87373f9d03da7862700c
pl23 <61396518+pl23@users.noreply.github.com>
2023-06-05 07:20:37 +0000

    Improved menu functionality and UX in main.py.

--- 0.00.01/main/main.py
+++ 0.00.01/main/main.py
@@ -1,5 +1,34 @@
 # main.py
 
+def display_main_menu():
+    menu = """
+=== Main Menu ===
+1. Load Game
+2. New Game
+3. Exit
+"""
+    print(menu)
+
+def main():
+    while True:
+        display_main_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            load_game()
+        elif choice == "2":
+            new_game()
+        elif choice == "3":
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+    print("Exiting the game.")
+
+if __name__ == "__main__":
+    main()
+
+
 def initialize_game():
     # Initialize the game here
     print("Game initialized")
