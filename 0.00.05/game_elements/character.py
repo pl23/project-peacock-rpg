74026e67ded6e6aa3771ee1dd39e29ed0d1bf4d3
pl23 <61396518+pl23@users.noreply.github.com>
2023-06-12 18:34:14 +0000

    a

--- 0.00.05/game_elements/character.py
+++ 0.00.05/game_elements/character.py
@@ -1,23 +1,72 @@
+class Character:
+    def __init__(self, name, race, character_class, experience_points):
+        self.name = name
+        self.race = race
+        self.character_class = character_class
+        self.experience_points = experience_points
+        self.ability_scores = {
+            'Strength': 0,
+            'Dexterity': 0,
+            'Constitution': 0,
+            'Intelligence': 0,
+            'Wisdom': 0,
+            'Charisma': 0
+        }
+        self.hit_points = {'current': 0, 'maximum': 0}
+        self.skills = {
+            'Acrobatics': 0,
+            'Animal Handling': 0,
+            'Arcana': 0,
+            'Athletics': 0,
+            'Deception': 0,
+            'History': 0,
+            'Insight': 0,
+            'Intimidation': 0,
+            'Investigation': 0,
+            'Medicine': 0,
+            'Nature': 0,
+            'Perception': 0,
+            'Performance': 0,
+            'Persuasion': 0,
+            'Religion': 0,
+            'Sleight of Hand': 0,
+            'Stealth': 0,
+            'Survival': 0
+        }
+        self.features_traits = []
+        self.alignment = ''
+        self.proficiency_bonus = 0
 
-class caracter:
-    __init__(self,data)
-    self.name : {'name':name}
-    self.
-    self.
-    self.
-    self.
-    self.
-    self.
-    self.
-    self.
-    self.
-    self.
-    self.
-    self.
+
+class Player(Character):
+    def __init__(self, name, race, character_class, experience_points):
+        super().__init__(name, race, character_class, experience_points)
+        self.background = ''
 
 
-class npc:
+class NPC(Character):
+    def __init__(self, name, race, character_class, experience_points, npc_type):
+        super().__init__(name, race, character_class, experience_points)
+        self.npc_type = npc_type
+        self.shopkeeper = False
+        self.manager = False
+        # Add more attributes specific to NPCs
+        # For example:
+        # self.occupation = ''
+        # self.dialogue = ''
+        # self.inventory = []
+        # self.quest = ''
+        # ...
 
+class ObjectCharacter:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
 
-class playre:
+    def interact(self):
+        # Implement interaction logic for the object character
+        pass
 
+    def examine(self):
+        # Implement examination logic for the object character
+        pass
--- 0.00.05/utilities/roll.py
+++ 0.00.05/utilities/roll.py
@@ -1,47 +1,43 @@
 import random
 
-def parse_dice_effect(dice_effect):
-    dice_effect = dice_effect.strip().upper()
-
-    if dice_effect[0] == 'D':
-        advantage = False
-        dice_parts = dice_effect[1:].split('D')
-    elif dice_effect[0] == 'A':
-        advantage = True
-        dice_parts = dice_effect[1:].split('D')
-    else:
-        advantage = None
-        dice_parts = dice_effect.split('D')
-
-    if len(dice_parts) != 2:
-        raise ValueError("Invalid dice format. Expected '1d6' format.")
-
-    die = dice_parts[0].strip()
-    sided = dice_parts[1].strip()
-
-    if not die.isdigit() or not sided.isdigit():
-        raise ValueError("Invalid dice format. Expected numeric values for die and sides.")
-
-    return die, sided, advantage
-
 def apply_advantage(rolls, advantage):
-    if advantage is True:
+    if advantage == "A":
         return max(rolls)
-    elif advantage is False:
+    elif advantage == "D":
         return min(rolls)
     else:
         return sum(rolls)
 
-def roll(die=None, sided=None, dice_effect=None, display_result=True, advantage=None):
-    if die is None and sided is None and dice_effect is None:
+def apply_proficiency(result, proficiency):
+    return result + proficiency
+
+def split_dice_effect(dice_effect):
+    dice_effect = dice_effect.strip()  # Remove leading/trailing whitespace
+
+    if 'A' in dice_effect:
+        parts = dice_effect.split('A')
+        die, sided = parts[1].strip().split('d')
+        advantage = 'A'
+    elif 'D' in dice_effect:
+        parts = dice_effect.split('D')
+        die, sided = parts[1].strip().split('d')
+        advantage = 'D'
+    else:
+        die, sided = dice_effect.strip().split('d')  # Strip whitespace from dice_effect
+        advantage = None
+
+    return int(die), int(sided), advantage
+
+def roll(die=None, sided=None, dice_effect=None, display_result=True, advantage=None, proficiency_bonus=0):
+    if not die and not sided and not dice_effect:
         raise ValueError("No input provided. Please specify die and sided values or dice_effect.")
 
     if dice_effect:
-        die, sided, advantage = parse_dice_effect(dice_effect)
+        die, sided, advantage = split_dice_effect(dice_effect)
     else:
         advantage = advantage
 
-    if not die or not sided:
+    if die is None or sided is None:
         raise ValueError("Missing die or sided value.")
 
     rolls = []
@@ -49,8 +45,27 @@ def roll(die=None, sided=None, dice_effect=None, display_result=True, advantage=
         rolls.append(random.randint(1, int(sided)))
 
     result = apply_advantage(rolls, advantage)
+    result_with_proficiency = apply_proficiency(result, proficiency_bonus)
 
     if display_result:
-        print(result)
+        print(result_with_proficiency)
 
-    return result
+    return result_with_proficiency
+
+# Testing the roll function with direct values and proficiency bonus
+roll(2, 6, proficiency_bonus=2)
+
+# Testing the roll function with '1d6' as the dice_effect and proficiency bonus
+roll(dice_effect='1d6', proficiency_bonus=-1)
+
+# Testing the roll function with advantage in the string and proficiency bonus
+roll(dice_effect='A1d6', proficiency_bonus=3)
+
+# Testing the roll function with disadvantage in the string and proficiency bonus
+roll(dice_effect='D1d6', proficiency_bonus=0)
+
+# Testing the roll function with separate die and sided values, advantage, and proficiency bonus
+roll(die=1, sided=6, advantage='A', proficiency_bonus=2)
+
+# Testing the roll function with separate die and sided values, disadvantage, and proficiency bonus
+roll(die=1, sided=6, advantage='D', proficiency_bonus=0)
